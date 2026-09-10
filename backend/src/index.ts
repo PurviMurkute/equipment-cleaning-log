733d0c64f5ceca2dd47cfc3c45400e08f01f6b68
PurviMurkute <purvimurkute51@gmail.com>
2026-09-10 11:03:46 +0530

    feat: server created and db connected

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import pool from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+    await pool.query("SELECT 1");
+
+    console.log("PostgreSQL connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("PostgreSQL connection failed:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
